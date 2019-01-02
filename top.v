e807f7f7b3d9ffde4528317dbd6ca602d6d6b91f
emard <vordah@gmail.com>
2019-01-03 00:33:25 +0100

    project initial files. For me it doesn't work, no LED blink

---
+++ top.v
@@ -0,0 +1,25 @@
+module top(
+    input clk_25mhz,
+    output [7:0] led,
+    output ftdi_rxd,
+    input ftdi_txd,
+    output wifi_gpio0
+);
+
+assign wifi_gpio0 = 1'b1;
+
+/*
+pll_12_50 pll(
+    .clki(clk_25mhz),
+    .clko(clk)
+);
+*/
+
+attosoc soc(
+    .clk(clk_25mhz),
+    .led(led),
+    .uart_tx(ftdi_rxd),
+    .uart_rx(ftdi_txd)
+);
+
+endmodule
